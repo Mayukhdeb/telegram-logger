135dde37c4d0a042f5e806f94c4c783e464ef0f0
mayukhdeb <mayukhmainak2000@gmail.com>
2025-08-13 16:59:55 -0400

    add progress bar

--- telegram_logger/__init__.py
+++ telegram_logger/__init__.py
@@ -1,1 +1,2 @@
 from .logger import TelegramLogger
+from .progress import tgtqdm
---
+++ telegram_logger/progress.py
@@ -0,0 +1,65 @@
+from .logger import TelegramLogger
+import time
+
+class tgtqdm:
+    """
+    A tqdm-like progress bar that logs to Telegram.
+    Usage:
+    ```
+    from telegram_logger import tgtqdm
+    for item in tgtqdm(iterable, api_token=TOKEN, chat_id=USER_ID, desc="Processing"):
+        # do something with item
+        # the progress bar will automatically update
+    ```
+    """
+    def __init__(self, iterable, json_filename: str = None, api_token: str=None, chat_id: int=None, desc: str = "", **kwargs):
+        self.iterable = iterable
+
+        if json_filename is None:
+            assert api_token is not None, "api_token must be provided"
+            assert chat_id is not None, "chat_id must be provided"
+            self.logger = TelegramLogger.from_json(filename=json_filename)
+        else:
+            self.logger = TelegramLogger(api_token=api_token, chat_id=chat_id)
+        self.total = len(iterable) if hasattr(iterable, '__len__') else None
+        self.current = 0
+        self.desc = desc
+        self.kwargs = kwargs
+
+    def __iter__(self):
+        start_time = time.time()
+
+        def format_eta(seconds):
+            if seconds >= 3600:
+                hours = int(seconds // 3600)
+                mins = int((seconds % 3600) // 60)
+                secs = int(seconds % 60)
+                return f"ETA: {hours}h {mins}m {secs}s"
+            elif seconds >= 60:
+                mins = int(seconds // 60)
+                secs = int(seconds % 60)
+                return f"ETA: {mins}m {secs}s"
+            else:
+                return f"ETA: {int(seconds)}s"
+
+        for item in self.iterable:
+            self.current += 1
+            elapsed = time.time() - start_time
+            if self.total:
+                percent = self.current / self.total
+                percent_text = f"{int(percent * 100):3d}%"
+                if self.current > 0:
+                    eta = elapsed / self.current * (self.total - self.current)
+                    eta_text = format_eta(eta)
+                else:
+                    eta_text = "ETA: --"
+                progress_line = f"[{self.current}/{self.total}] {percent_text}\n{eta_text}"
+            else:
+                progress_line = f"[{self.current}]"
+
+            if self.desc:
+                message = f"{self.desc}: {progress_line}"
+            else:
+                message = progress_line
+            self.logger.log(message=message, timestamp=True)
+            yield item
